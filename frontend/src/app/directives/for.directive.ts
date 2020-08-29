b0c0d307968bdc581d3a0b64c3632f2d77c8f185
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-08-29 14:23:29 -0300

    Direcivas na prática 02 - Diretiva estrutural.

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+
+  @Input('myForEm') numbers: number[]
+
+  // tslint:disable-next-line: no-empty
+  constructor(private container: ViewContainerRef, private template: TemplateRef<any>) { }
+
+  ngOnInit(): void {
+    for (const number of this.numbers) {
+      this.container.createEmbeddedView(this.template, { $implicit: number });
+    }
+  }
+
+}
