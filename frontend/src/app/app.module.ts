671d908b8e07640333ac048b6d4c3d6fcf13f461
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-08-29 16:51:20 -0300

    Implementação do Product Service.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,10 +15,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
+
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+
 
 
 @NgModule({
@@ -41,7 +45,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
