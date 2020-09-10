c190c9a9fe2d3f8eb232b3f13288806737493885
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-09-09 22:37:56 -0300

    Implementação componente product delete.

---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,41 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product: Product;
+  idProduct: string;
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.idProduct = this.route.snapshot.paramMap.get('id');
+    this.productService.readById(this.idProduct).subscribe(product => {
+      this.product = product;
+    });
+
+  }
+
+  deleteProduct(): void {
+    this.productService.delete(this.idProduct).subscribe(() => {
+      this.productService.showMessage('Produto excluido com sucesso.');
+      this.goToProductListPage();
+    });
+  }
+
+  cancel(): void {
+    this.goToProductListPage();
+  }
+
+  goToProductListPage(): void {
+    this.router.navigate(['/products']);
+  }
+
+}
